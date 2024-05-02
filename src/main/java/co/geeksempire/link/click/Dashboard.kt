503106ec6db64ed1025aeb12e6077fabdcdbc335
Elias Fazel <elias.fazel@gmail.com>
2024-05-02 10:52:52 -0400

    Link Click

---
+++ src/main/java/co/geeksempire/link/click/Dashboard.kt
@@ -0,0 +1,21 @@
+package co.geeksempire.link.click
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.link.click.databinding.DashboardBinding
+
+class Dashboard : AppCompatActivity() {
+
+    private lateinit var dashboardBinding: DashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dashboardBinding = DashboardBinding.inflate(layoutInflater)
+        setContentView(dashboardBinding.root)
+
+
+
+    }
+
+}
---
+++ src/main/java/co/geeksempire/link/click/Service/AccessibilityServices.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 5/4/20 12:51 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+package co.geeksempire.link.click.Service
+
+import android.accessibilityservice.AccessibilityService
+import android.content.Intent
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+
+class AccessibilityServices : AccessibilityService() {
+
+
+    override fun onServiceConnected() {}
+
+    override fun onAccessibilityEvent(accessibilityEvent: AccessibilityEvent) {
+
+        when (accessibilityEvent.eventType) {
+            AccessibilityEvent.TYPE_VIEW_CLICKED -> {
+
+                accessibilityEvent.source?.let {
+
+                    val source = accessibilityEvent.source!!
+
+                    try {
+
+                        Log.i(this@AccessibilityServices.javaClass.simpleName, source.text.toString())
+
+                        Log.i(this@AccessibilityServices.javaClass.simpleName, source.hintText.toString())
+
+                        Log.i(this@AccessibilityServices.javaClass.simpleName, source.contentDescription.toString())
+
+                    } catch (e: Exception) {}
+
+                }
+
+            }
+        }
+
+    }
+
+    override fun onInterrupt() {
+        startService(Intent(applicationContext, AccessibilityServices::class.java))
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
